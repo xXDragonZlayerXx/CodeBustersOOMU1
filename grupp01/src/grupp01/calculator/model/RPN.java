240e34ecec948b025eddaf038b1a75b8325bcaef
optimusprime <optimusprime@DESKTOP-KMK55FI>
2016-09-22 21:19:14 +0200

    final

--- grupp01/src/grupp01/calculator/model/RPN.java
+++ grupp01/src/grupp01/calculator/model/RPN.java
@@ -14,23 +14,23 @@ import java.lang.IllegalArgumentException;
  *
  * @author optimusprime94
  */
-public class RPN implements Token  {
+public class RPN implements Token {
 
     protected static Stack<Token> st = new Stack<>();
-    
-    public RPN(){
-            
+
+    public RPN() {
+
     }
 
     // Skapa och lägga om tokens i stacken operator och operander
     public void InputStackTokens(String stringExpression) throws IllegalArgumentException {
-       
-           String str = new String(stringExpression);
-           
-           for (String token : str.split(" ")) {
-                if(token.matches("[-+]?\\d*\\.?\\d+"))
-                 st.push(new Operand(token));
-                else{
+
+        String str = new String(stringExpression);
+
+        for (String token : str.split(" ")) {
+            if (token.matches("[-+]?\\d*\\.?\\d+")) { // Kollar om token är godkänd 
+                st.push(new Operand(token));
+            } else {
                 switch (token) {
                     case "+":
                         st.push(new PlusOperator());
@@ -45,9 +45,9 @@ public class RPN implements Token  {
                         st.push(new DivisionOperator());
                         break;
                     case "%":
-                       st.push(new ModulusOperator());
+                        st.push(new ModulusOperator());
                         break;
-                        
+
                     default:
                         throw new IllegalArgumentException("Error: InvalidTokenException");
                 }
@@ -55,30 +55,30 @@ public class RPN implements Token  {
         }
     }
 
- 
-
     @Override
     /* Rekursiv Metod som ränkar ut 
-    * Uttrycken i Stacken och returnerar det
-    *
-    */
+     * Uttrycken i Stacken och returnerar det
+     *
+     */
     public double EvaluateToken() throws Exception {
 
         double result = 0.0;
-        
+
         if (!st.empty()) {
             result = st.pop().EvaluateToken();
         }
-        if(!st.empty()){
+        if (!st.empty()) {
             throw new IllegalArgumentException("Error: InvalidOperationException");
         }
-        
+
         return result;
     }
-    // Metod som "rensar" stacken efter användning
-    public void clearStack(){
+
+    /*
+     * Metod som "rensar" stacken efter användning eftersom 
+     * stacken är static så behövs denna vid felinmatning.
+     */
+    public void clearStack() {
         RPN.st.clear();
     }
 }
-
-
--- grupp01/src/grupp01/calculator/view/CommandView.java
+++ grupp01/src/grupp01/calculator/view/CommandView.java
@@ -12,46 +12,45 @@ import java.util.Scanner;
  *
  * @author optimusprime
  */
-public class CommandView extends View{
+public class CommandView extends View {
+
     RPN calc = new RPN();
-    
+
     @Override
-        public void DisplayView(){
-    System.out.println("Ange RPN uttryck <retur> (tom sträng = avsluta): ");
-    
+    public void DisplayView() {
+        System.out.println("Ange RPN uttryck <retur> (tom sträng = avsluta): ");
+
     }
 
     @Override
-     /*
+    /*
     * Uview, metod som väntar på 
     * input från användare och körs
     * tills användaren matar in en tom sträng
-    */
-    public void Uview(String[] args){
-        
-        
+     */
+    public void Uview(String[] args) {
+
         Scanner input = new Scanner(System.in);
         String str;
-        
-        while (true) {
-            try{
-            System.out.println("Ange RPN uttryck <retur> (tom sträng = avsluta): ");
-            str = input.nextLine();
-            if (str.isEmpty()) {
-                System.out.println("Användaren avslutade programmet");
-                System.exit(0);
-            } else {
-                calc.InputStackTokens(str);
-                System.out.println("Svar: " + calc.EvaluateToken());
-            }
 
-        }catch(Exception e){
+        while (true) {
+            try {
+                System.out.println("Ange RPN uttryck <retur> (tom sträng = avsluta): ");
+                str = input.nextLine();
+                if (str.isEmpty()) {
+                    System.out.println("Användaren avslutade programmet");
+                    System.exit(0);
+                } else {
+                    calc.InputStackTokens(str);
+                    System.out.println("Svar: " + calc.EvaluateToken());
+                }
+
+            } catch (Exception e) {
                 calc.clearStack();
                 System.out.println(e.getMessage());
-        
-        }
-           
-     
+
             }
+
+        }
     }
 }
